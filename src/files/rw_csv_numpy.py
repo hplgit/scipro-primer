5917e1c20c03e38a0db9fe775bcc04fb31e283b0
Hans Petter Langtangen <hpl@simula.no>
2012-10-24 06:08:09 +0200

    update

--- src/files/rw_csv_numpy.py
+++ src/files/rw_csv_numpy.py
@@ -4,11 +4,12 @@ table = [row for row in csv.reader(infile)]
 infile.close()
 
 # Convert subtable of numbers (string to float)
+import numpy
 subtable = [[float(c) for c in row[1:]] for row in table[1:]]
 
 data = {'column headings': table[0][1:],
         'row headings': [row[0] for row in table[1:]],
-        'array': array(subtable)}
+        'array': numpy.array(subtable)}
 
 # Add a new row with sums
 data['row headings'].append('sum')
@@ -17,13 +18,13 @@ data['column sum'] = [sum(a[:,c]) for c in range(a.shape[1])]
 
 outfile = open('budget2.csv', 'w')
 writer = csv.writer(outfile)
-# Aurn data dictionary into a nested list first (for easy writing)
+# Turn data dictionary into a nested list first (for easy writing)
 table = a.tolist()   # transform array to nested list
 table.append(data['column sum'])
 table.insert(0, data['column headings'])
-# Axtend table with row headings (a new column)
-table = [table[r].insert(0, data['row headings'][r]) \
-         for r in range(len(table))]
+# Extend table with row headings (a new column)
+[table[r+1].insert(0, data['row headings'][r]) \
+ for r in range(len(table)-1)]
 for row in table:
     writer.writerow(row)
 outfile.close()
